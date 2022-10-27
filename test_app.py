8c91104dc311162afe273562b3619e2b6979ffe5
Tim Chiang <tim@Tims-MacBook-Pro.local>
2022-10-27 15:45:33 +0800

    18.0

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from main import hello_world
 class TestHelloApp(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(hello_world(), "Hello World!\n")
+        self.assertEqual(hello_world(), "Hello Production World")
 
 
 if __name__ == '__main__':
