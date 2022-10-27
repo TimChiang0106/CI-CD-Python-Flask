2fa0105caba0902782b6bbed808818c48800c9be
Tim Chiang <tim@Tims-MacBook-Pro.local>
2022-10-27 12:43:35 +0800

    2.5

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! version 2.0</p>"
+    return "<p>Hello, World! version prod</p>"
+
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
