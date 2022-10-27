fc0f67778440b0b479692bdb7a169a5229800388
Tim Chiang <tim@Tims-MacBook-Pro.local>
2022-10-27 15:35:17 +0800

    12.0

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! version 2.0</p>"
+    name = os.environ.get("NAME", "World")
+    return "Hello Production {}!".format(name)
 
-if __name__ == '__main__':
-   app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from main import hello_world
+
+
+class TestHelloApp(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello_world(), "Hello World!\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
